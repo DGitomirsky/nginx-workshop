252fed0f95c49777fabc81cfd1ec8cbefaab73fe
Dominika Gitomirsky <48725512+DGitomirsky@users.noreply.github.com>
2023-04-04 11:44:57 +0000

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+import logo from './logo.svg';
+import './App.css';
+
+function App(_res,_req) {
+  const [title] = useState(`${window._env_.API_URL}`);
+  // Will run on first load $ every time the title changes
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+  return (   
+    <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Message: {window._env_.API_URL}
+          </p>
+          <p>
+            HOST: {window._env_.HOSTNAME}
+          </p>
+          <a
+            className="link"
+            href="/backend1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Test backend 1
+          </a>
+          <a
+            className="link"
+            href="/backend2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Test backend 2
+          </a>
+        </header>
+      </div>
+  );
+}
+
+export default App;
